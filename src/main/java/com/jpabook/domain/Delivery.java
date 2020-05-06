46ba4e177b9e4ce5fa50a0961b2a5a497220cf44
youdongkwan <dkyou7@naver.com>
2020-05-06 13:53:46 +0900

    2차 도메인 구성 완료

--- src/main/java/com/jpabook/domain/Delivery.java
+++ src/main/java/com/jpabook/domain/Delivery.java
@@ -4,22 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter @Setter
-public class Member {
+public class Delivery {
 
     @Id @GeneratedValue
+    @Column(name = "delivery_id")
     private Long id;
 
-    private String name;
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
 
     @Embedded
     private Address address;
 
-    // 연관관계의 종속자
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
 }
--- src/main/java/com/jpabook/domain/Order.java
+++ src/main/java/com/jpabook/domain/Order.java
@@ -13,16 +13,35 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
+    // 연관관계의 주인
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-//    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-//    private List<OrderItem> orderItems = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinColumn(name = "deliverty_id")
+    private Delivery delivery;
 
     private LocalDateTime orderDate;    // 주문 시간
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태[ORDER,CANCEL]
 
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this); // 이렇게만 했는데 추가가 된다고? 파라메터 던진것만 받아서 처리했는데?
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
