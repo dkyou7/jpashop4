f47b600d3539ee88a5ebf76ece431cc15ab72ab0
youdongkwan <dkyou7@naver.com>
2020-05-06 10:44:48 +0900

    초기 설정 완료

---
+++ README.md
@@ -0,0 +1,12 @@
+# jpa test
+
+## 20.05.06
+- README.md 추가
+- pom.xml 작성
+- application.yml 작성
+- main.java 작성
+- DB 연동 테스트 완료
+- spring-boot-devtools 적용 완료
+    - build>recompile 해주자.
+- 
+
---
+++ src/main/java/com/jpabook/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.jpabook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","유동관");
+        return "hello";
+    }
+}
