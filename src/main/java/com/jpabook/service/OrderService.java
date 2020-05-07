761b3c910289e375dfd93ffd477c5738b126bbd4
youdongkwan <dkyou7@naver.com>
2020-05-07 17:46:12 +0900

    주문 기능 개발

---
+++ src/main/java/com/jpabook/service/OrderService.java
@@ -0,0 +1,49 @@
+package com.jpabook.service;
+
+import com.jpabook.domain.*;
+import com.jpabook.domain.item.Item;
+import com.jpabook.repository.ItemRepository;
+import com.jpabook.repository.MemberRepository;
+import com.jpabook.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemService itemService;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemService.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item,item.getPrice(),count);
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+}
---
+++ src/test/java/com/jpabook/service/OrderServiceTest.java
@@ -0,0 +1,7 @@
+package com.jpabook.service;
+
+import static org.junit.Assert.*;
+
+public class OrderServiceTest {
+
+}
