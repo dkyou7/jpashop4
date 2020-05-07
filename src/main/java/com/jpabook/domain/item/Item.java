052e957e58bd8fd2e338a55677ebe58dfb84db86
youdongkwan <dkyou7@naver.com>
2020-05-07 10:23:56 +0900

    상품 관련 개발 완료

--- src/main/java/com/jpabook/domain/item/Item.java
+++ src/main/java/com/jpabook/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpabook.domain.item;
 
 import com.jpabook.domain.Category;
+import com.jpabook.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need More stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
