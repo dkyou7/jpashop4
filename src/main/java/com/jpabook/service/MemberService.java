d07edcf025b5edeb9b46fe129777cfc4b21bf54b
youdongkwan <dkyou7@naver.com>
2020-05-26 18:03:59 +0900

    뭔가 이상하지만 test 도 잘 되고 실행도 잘 된다. 왜 빨간줄이 생기는걸까??

--- src/main/java/com/jpabook/service/MemberService.java
+++ src/main/java/com/jpabook/service/MemberService.java
@@ -42,4 +42,10 @@ public class MemberService {
     public Member findOne(Long memberId){
         return memberRepository.findOne(memberId);
     }
+
+    @Transactional
+    public void update(Long id,String name){
+        Member mem = memberRepository.findOne(id);
+        mem.setName(name);
+    }
 }
