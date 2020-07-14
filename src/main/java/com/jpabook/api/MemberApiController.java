4627bad1af0a14ae3b588704e65057ef0ff1ef65
dkyou7 <dkyou7@naver.com>
2020-07-14 11:40:08 +0900

    회원등록 API 학습

--- src/main/java/com/jpabook/api/MemberApiController.java
+++ src/main/java/com/jpabook/api/MemberApiController.java
@@ -8,25 +8,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
+//@Controller + @ResponseBody = @RestController
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
 
+    // @RequestBody : json 객체를 Member로 바꿔줌
+    // 엔티티에 종속적으로 변한다. 엔티티 따로, 컨트롤러 따로 개발할 때, 스팩이 변하면 에러를 찾기 힘들다.
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
-
     @Data
-    static class CreateMemberRequest{
-        private String name;
-    }
-
-    @Data
-    class CreateMemberResponse{
+    static class CreateMemberResponse{
         private Long id;
 
         public CreateMemberResponse(Long id){
@@ -34,14 +32,30 @@ public class MemberApiController {
         }
     }
 
+
+
+    // 엔티티에 종속적이지 않게 DTO를 만들어서 처리해준다.
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+
+        // 엔티티와의 매핑하는데 번거롭지만, api 스팩별 관리가 가능해지므로 좋다.
         Member member = new Member();
         member.setName(request.getName());
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @Data
+    static class CreateMemberRequest{
+
+        @NotEmpty   // 무조건 값이 있어야 한다.
+        // 여기서 valid 처리를 해준다.
+        private String name;
+    }
+
+
+
 
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request){
